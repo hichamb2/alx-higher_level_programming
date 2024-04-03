68dbed7d7d695cfc2f1c44d909c0cac412bd8743
hichamb2 <beihaqihicham@gmail.com>
2024-04-03 00:18:53 +0000

    add

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(f"{my_list[i]}", end="")
             j += 1
         print()
-    except:
+    except IndexError:
         print()
     return (j)
