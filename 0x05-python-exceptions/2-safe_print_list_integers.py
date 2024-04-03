01569a428a87d1b443e85d06d9d64acf696c53d9
hichamb2 <beihaqihicham@gmail.com>
2024-04-03 02:09:31 +0000

    add

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     j = 0
     try:
         for i in range(x):
-            if type(my_list[i]) != int:
+            if type(my_list[i]) is not int:
                 continue
             print("{:d}".format(my_list[i]), end="")
             j += 1
