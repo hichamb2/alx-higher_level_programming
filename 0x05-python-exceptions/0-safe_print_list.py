ab1a6fb06ca5e05032da215d4d45d81e78d1a985
hichamb2 <beihaqihicham@gmail.com>
2024-04-03 00:05:56 +0000

    add

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    try:
+        for i in range(x):
+            print(f"{my_list[i]}", end="")
+            j += 1
+        print()
+    except:
+        print()
+    return (j)
