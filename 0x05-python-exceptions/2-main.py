9897495d5fa791658f7d498db82eedf813facc65
hichamb2 <beihaqihicham@gmail.com>
2024-04-13 21:56:20 +0000

    commit for revising

--- 0x05-python-exceptions/2-main.py
+++ 0x05-python-exceptions/2-main.py
@@ -11,3 +11,7 @@ nb_print = safe_print_list_integers(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
+my_list = [1, 2, 3, 4]
+x = len(my_list) + 4
+nb_print = safe_print_list_integers(my_list, x)
+print("{:d}".format(nb_print))
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,4 @@ def safe_print_list_integers(my_list=[], x=0):
                 j += 1
         except (IndexError, ValueError, TypeError):
             continue
-    print()
     return (j)
