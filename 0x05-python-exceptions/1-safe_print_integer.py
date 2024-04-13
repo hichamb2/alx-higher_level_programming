3f53607a71509680435ea479b7c0f57e436f69d3
hichamb2 <beihaqihicham@gmail.com>
2024-04-13 21:31:39 +0000

    add

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,5 @@ def safe_print_integer(value):
         if isinstance(value, int):
             print("{:d}".format(value))
             return (True)
-    except TypeError :
+    except TypeError:
         return (False)
