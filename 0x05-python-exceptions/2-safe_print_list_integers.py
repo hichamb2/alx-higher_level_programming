2224d3ff27958107539f80ec0aec097660606f3b
hichamb2 <beihaqihicham@gmail.com>
2024-04-13 21:57:31 +0000

    commit for revising

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,6 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if j < x:
                 print("{:d}".format(i), end="")
                 j += 1
-        except (IndexError, ValueError, TypeError):
+        except (ValueError, TypeError):
             continue
+    print()
     return (j)
