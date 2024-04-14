90cad66c4cc4d5dab09fdf9d9576f8a468399ca4
hichamb2 <beihaqihicham@gmail.com>
2024-04-15 00:24:57 +0100

    commit for revising

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -25,9 +25,7 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-            return
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end="")
             print()
-        return ()
