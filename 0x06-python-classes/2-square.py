566d9863381ea3eb55b2d5532bffb12dc7f76e3d
hichamb2 <beihaqihicham@gmail.com>
2024-04-14 19:52:37 +0100

    commit for revising

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """defines a square"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         elif size < 0:
