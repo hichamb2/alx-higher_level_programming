90f5653ca4aa54476acee0d9e5947322ff106bdd
hichamb2 <beihaqihicham@gmail.com>
2024-04-15 01:30:58 +0100

    commit for revising

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,7 +30,11 @@ class Square:
     @position.setter
     def position(self, value):
         """sitter of ?? to position"""
-        if (len(self.__position != 2) or not isinstance(self.__position[0], int) or not isinstance(self.__position[1], int) not isinstance(value, tuple) or value[0] < 0  or value[1] < 0:
+        if len(self.__position) != 2 \
+                or not isinstance(self.__position[0], int) \
+                or not isinstance(self.__position[1], int) \
+                or not isinstance(value, tuple) \
+                or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
