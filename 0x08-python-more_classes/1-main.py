e5ac4bc5659434572e4ee67e8a1926c8d7e121a8
hichamb2 <beihaqihicham@gmail.com>
2024-04-17 00:06:38 +0100

    commit for revising

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 Rectangle = __import__('1-rectangle').Rectangle
 
+my_rectangle = Rectangle(2, 3)
+print(my_rectangle.width)
+print(my_rectangle.height)
 try:
     my_rectangle = Rectangle("2", 3)
 except Exception as e:
