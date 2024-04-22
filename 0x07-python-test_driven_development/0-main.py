54eac3f85341b804d04da1b562eb643dd60cfd32
hichamb2 <beihaqihicham@gmail.com>
2024-04-23 00:50:14 +0100

    commit for revising

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -3,7 +3,6 @@ add_integer = __import__('0-add_integer').add_integer
 
 print(add_integer(1, 2))
 print(add_integer(100, -2))
-print(add_integer(2))
+print("#" * 33)
+print(add_integer(None))
 print(add_integer(100.3, -2))
-m = __import__("0-add_integer").add_integer.__doc__
-print(m)
