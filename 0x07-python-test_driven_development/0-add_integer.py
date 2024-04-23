d9fd4bf585f46694c7e7d13c62cee5d63a1b8ef1
hichamb2 <beihaqihicham@gmail.com>
2024-04-23 01:04:09 +0100

    commit for revising

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,7 +2,7 @@
 """define a function"""
 
 
-def add_integer(a, b):
+def add_integer(a, b=98):
     """function/method that add 2 integers"""
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
