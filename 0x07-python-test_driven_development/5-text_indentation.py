fe32f93258f74b8be2e96112bb7de1902341a1a2
hichamb2 <beihaqihicham@gmail.com>
2024-04-28 00:58:34 +0100

    commit for revising

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""
-This is the "5-test_indentation" module.
-The 5-text_indentation module supplies one function, text_indentation(text).
-"""
+"""define a function"""
 
 
 def text_indentation(text):
-    """splits a text into lines along "?", ":", "." followed by 2 new lines"""
-    if type(text) is not str:
+    """function/method that add 2 integers"""
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-    flag = 0
-    for a in text:
-        if flag == 0:
-            if a == ' ':
-                continue
-            else:
-                flag = 1
-        if flag == 1:
-            if a == '?' or a == '.' or a == ':':
-                print(a)
-                print()
-                flag = 0
-            else:
-                print(a, end="")
+    i = 0
+    while(i  < len(text)):
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print("{}".format(text[i]))
+            print("\n", end="")
+            if text[i + 1] == ' ':
+                i = i + 1
+        else:
+            print("{}".format(text[i]), end="")
+        i += 1
