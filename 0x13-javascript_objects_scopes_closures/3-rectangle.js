4e94257b47e98524040e521d4cff9078a7e4f770
hichamb2 <beihaqihicham@gmail.com>
2024-07-02 12:57:57 +0100

    add

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        console.log('x');
+      }
+    }
+  }
+}
+module.exports = Rectangle;
